c4df282982591475f9c60bc7d33209549883dca2
Eryashgoyal <yashgoyal91111@gmail.com>
2023-08-09 13:38:38 +0530

    project structure added

---
+++ auth_apis/src/main/java/com/yash/auth_apis/AuthApisApplication.java
@@ -0,0 +1,13 @@
+package com.yash.auth_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApisApplication.class, args);
+	}
+
+}
---
+++ auth_apis/src/test/java/com/yash/auth_apis/AuthApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.auth_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
